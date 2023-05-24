924526bafba43fb381eeafb369ce42d11e6ef179
robertIsaac <dev.robert.isaac@gmail.com>
2023-05-25 00:39:53 +0300

    reproduce the problem
    try to run `nx lint enforce-module-boundaries-lint-reprod`, it will never finish up

--- apps/enforce-module-boundaries-lint-reprod/src/app/app.service.ts
+++ apps/enforce-module-boundaries-lint-reprod/src/app/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '.prisma/client';
+//                    ^
+// import { User } from '@prisma/client'; // this will work
+//                       ^
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    const user: User = {
+      id: 0,
+      username: ''
+    };
+    console.log(user);
     return { message: 'Hello API' };
   }
 }
